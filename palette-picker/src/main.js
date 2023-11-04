0d84479d98ae2697a85cdf998cbe2674cebec442
David <dcobos00@gmail.com>
2023-11-04 16:31:03 -0400

    added delete feature and moved styling from js to css file

--- palette-picker/src/main.js
+++ palette-picker/src/main.js
@@ -16,25 +16,20 @@ const createColorOption = (color) => {
   copyButton.className = 'copyButton'
 
   const exampleBackground = document.createElement('div')
-  exampleBackground.style.width = '60%'
+  exampleBackground.className = 'exampleBackground'
 
   const whiteBox = document.createElement('div')
-  whiteBox.style.backgroundColor = 'white'
-  whiteBox.style.height = "30px";
-  whiteBox.style.marginBottom = '-10px'
+  whiteBox.className = 'whiteBox'
   
 
   const blackBox = document.createElement('div')
-  blackBox.style.backgroundColor = 'black'
-  blackBox.style.height = '30px'
-  blackBox.style.marginTop = '-10px'
+  blackBox.className = 'blackBox'
   
   const color1 = document.createElement('p')
+  color1.className = 'newColor'
   color1.style.backgroundColor = color
   color1.innerHTML = `Text <span>Example</span>`
-  color1.style.display = 'inline'
-  color1.style.padding = '10px'
-  color1.querySelector('span').style.color = 'black'
+  
 
   exampleBackground.append(whiteBox, color1, blackBox)
   option.append(exampleBackground, copyButton)
@@ -44,22 +39,15 @@ const createColorOption = (color) => {
 
 const createPalette = (colors, title, temp) => {
 
-
   const newPalette = document.createElement('div')
-  newPalette.style.width = '25%'
-  newPalette.style.textAlign = 'center'
-  newPalette.style.border = '1px solid white'
-  newPalette.style.backgroundColor = '#333'
-  newPalette.style.margin = '0 2.5%'
-  
+  newPalette.className = 'newPalette'
 
   const paletteTitle = document.createElement('h3')
   paletteTitle.textContent = title
 
   const paletteTemp = document.createElement('p')
   paletteTemp.textContent = temp
-  paletteTemp.style.margin = 'auto'
-  paletteTemp.style.padding = '10px'
+  paletteTemp.className = 'paletteTemp'
   switch(temp){
     case 'neutral':
       paletteTemp.style.backgroundColor = '#555';
@@ -74,6 +62,9 @@ const createPalette = (colors, title, temp) => {
 
   const deleteButton = document.createElement('button')
   deleteButton.textContent = 'Delete Palette'
+  deleteButton.onclick = function(){
+    newPalette.remove()
+  }
 
   const option1 = createColorOption(colors[0])
   const option2 = createColorOption(colors[1])
